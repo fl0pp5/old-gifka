cf7d97a4b6ba3f856e36735ce2c0c409267290ad
fl0pp5 <fl0pp5@mail.ru>
2022-10-12 20:16:46 +0300

    add possibility to upload image from admin panel

--- oldgifka/pool/models.py
+++ oldgifka/pool/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class Post(models.Model):
+    image = models.ImageField(upload_to='images/')
+    is_published = models.BooleanField(default=False)
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=120)
+    post = models.ForeignKey('Post', on_delete=models.CASCADE)
